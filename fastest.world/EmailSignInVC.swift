3011128ae3ca4c80803475ea35f4214ee9f09cdc
RamR <codeman53@users.noreply.github.com>
2016-09-12 20:52:13 -0500

    Added Segue to Email Login Page and Finished Email Login Page UI

---
+++ fastest.world/EmailSignInVC.swift
@@ -0,0 +1,22 @@
+//
+//  EmailSignInVC.swift
+//  fastest.world
+//
+//  Created by RamR on 9/12/16.
+//  Copyright © 2016 VikramR. All rights reserved.
+//
+
+import UIKit
+
+class EmailSignInVC: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+
+}
